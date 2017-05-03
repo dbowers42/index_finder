c658f8ff106d003b7a6f4e08a044822090251a8e
dbowers42 <dbowers.github@gmail.com>
2017-05-03 10:27:13 -0600

    create funciton that returns the index of the first non-duplicate item in an array

--- lib/index_finder.ex
+++ lib/index_finder.ex
@@ -1,19 +1,34 @@
 defmodule IndexFinder do
   def extract(items) do
-      IO.inspect items
-      IO.puts String.duplicate("-", 100)
+    IO.puts String.duplicate("-", 100)
     items
-    |> Enum.map(fn t -> {elem(t, 1)} end)
+    |> IO.inspect
 
   end
 
 
   def find_index(items) when is_list(items) do
+    buckets = %{}
+
+
     items
     |> Enum.with_index()
     |> Enum.group_by(&elem(&1, 0))
-    |> Map.values()
-    |> Enum.map(&extract/1)
+    |> Enum.unzip()
+    |> elem(1)
+    |> Enum.filter(&Enum.count(&1) == 1)
+    |> Enum.map(&List.first(&1))
+    |> Enum.sort(fn (a, b) -> elem(a, 1) <= elem(b, 1) end)
+    |> List.first()
+
+
+
+
+
+
+
+
+
 
 
 
@@ -22,7 +37,7 @@ defmodule IndexFinder do
   end
 
   def start(_type, _args) do
-    data = ["B", "C", "D", "B", "A", "F", "A", "D", "A"]
+    data = ["B", "C", "H", "D", "G", "B", "A", "C", "F", "A", "D", "A"]
     find_index(data) |> IO.inspect
     {:ok, self()}
   end
